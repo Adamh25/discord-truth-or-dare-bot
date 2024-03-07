fae523e84bab53ee4a024cffaa0ee99a2748af9f
Adamh25 <162492139+Adamh25@users.noreply.github.com>
2024-03-07 07:54:23 +0000

    Create game.py
    The use of the 'Game' class allows the adding of players as well as determining whose turn it is.

---
+++ game.py
@@ -0,0 +1,24 @@
+# game.py
+
+import random
+from constants import TRUTH_PROMPTS, DARE_PROMPTS
+
+class Game:
+    def __init__(self):
+        self.players = []
+        self.current_player_index = 0
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def get_current_player(self):
+        return self.players[self.current_player_index]
+
+    def next_turn(self):
+        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+
+    def get_truth_prompt(self):
+        return random.choice(TRUTH_PROMPTS)
+
+    def get_dare_prompt(self):
+        return random.choice(DARE_PROMPTS)
